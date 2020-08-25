5724ba33b3571e5863a791261ee9478fc9892d7f
federico.quaglia <federico.quaglia@external.thalesgroup.com>
2020-08-25 21:30:19 +0200

    first commit

---
+++ ajax/ottieni_piatti_in_produzione_incorso.ajax.php
@@ -0,0 +1,47 @@
+<?php
+	require_once '../include/core.inc.php';
+	$link=connectToDb();
+	$portate=array();
+		$idprg=0;
+		$idprg_next=0;
+		$query_idprog="SELECT min(idprogrammazione) as idprg FROM programmazioneordini WHERE stato = 2";
+		$result_idprog = mysqli_query($link, $query_idprog) or die("#error#".mysqli_error($link));
+	    while ($row_idprg = mysqli_fetch_assoc($result_idprog)) {
+	    	$idprg=$row_idprg['idprg'];
+	    }
+	    /*$query_next_idprg="SELECT idprogrammazione FROM programmazioneordini 
+	    					WHERE categoria=$cat AND stato = 2 AND idprogrammazione>$idprg";
+		if($idprg>0){
+			$result_idprog_next = mysqli_query($link, $query_next_idprg) or die("#error#".mysqli_error($link));
+		    while ($row_idprg_next = mysqli_fetch_assoc($result_idprog_next)) {
+		    	$idprg_next=$row_idprg_next['idprogrammazione'];
+		    }
+		}*/
+	    if($idprg>0){
+	    	$query="SELECT *,COUNT(id) AS nr FROM programmazioneordini 
+			WHERE idprogrammazione=$idprg 
+			AND stato=2
+			AND categoria IN ('primo','secondo','contorno')
+			GROUP BY portata, tavolo ,indice, idprogrammazione
+			ORDER BY idprogrammazione, tavolo, portata";
+
+			$result = mysqli_query($link, $query) or die("#error#".mysqli_error($link));
+		    while ($row = mysqli_fetch_assoc($result)) {
+		    	array_push($portate, array('portata' => $row['portata'], 
+		    								'indice' => $row['indice'],
+		    								'tavolo' => $row['tavolo'],
+		    								'nr' => $row['nr'],
+		    								'idprg' => $row['idprogrammazione'],
+											'cat' => $row['categoria']));
+		    }  
+	    }
+		
+
+	disconnetti_mysql($link, NULL); #visto che non ho un result_set gli passo NULL.. nella funzione in core.in.php ho aggiunto il controllo
+
+	echo json_encode($portate);
+	
+
+?>
+
+ 
---
+++ js/loadData.js
@@ -0,0 +1,122 @@
+var primi= new Array();
+var secondi = new Array();
+var primiId=new Array();
+var secondiId=new Array();
+var contentId=new Array();
+$(document).ready(function(){
+
+	loadData();
+
+	setInterval(loadData, 5000);
+	
+	
+	
+});
+
+function reload(){
+	setTimeout("location.reload(true);", 60000);
+}
+
+window.onload=reload();
+
+function loadData(){
+	$.ajax({
+        type: "GET",
+        url: "ajax/ottieni_piatti_in_produzione_incorso.ajax.php",
+        dataType:"json",        
+        timeout: 4000,
+        success:function(response){
+            if (response) {
+
+                showData(response,'incorso');
+                if(response.length > 0){
+                	loadDataNext(parseInt(response[0].idprg)+1);
+                }
+            }
+            else {
+                // Process the expected results...
+            }
+        }
+
+    });
+}	
+
+function loadDataNext(index){
+	$.ajax({
+        type: "GET",
+        url: "ajax/ottieni_piatti_in_produzione_next.ajax.php",
+        dataType:"json",        
+        timeout: 4000,
+        data:{
+        	index: index
+        },
+        success:function(response){
+            if (response) {
+
+                showData(response,'next');
+                	
+            }
+            else {
+                // Process the expected results...
+            }
+        }
+
+    });
+}
+
+function showData(data,time){
+	$('#content-'+time).empty();
+	$('#primo-'+time).empty();
+	$('#secondo-'+time).empty();
+	var arrays=_.groupBy(data, 'idprg');
+	var gets=new Array();
+	$.each(arrays, function(index, arr) {
+		
+		$('#content-'+time).append('<p class="combine">'+'COMBINE N. '+index+'</p>');
+		
+
+		
+		
+		$.each(arr, function(idx,value){
+			var idDiv=value.tavolo+value.indice+value.portata.replace(/ /g, '')+value.idprg;
+			gets.push(idDiv);
+			//if(!_.includes(contentId, idDiv)){
+				var _class = 'blue';
+				if(value.tavolo<200){
+					_class = 'yellow';
+				}
+				$('#'+value.cat+'-'+time).append('<div class="col-11 my-1 space '+_class+'" id="'+idDiv+'">'
+								+'Tav. '+value.tavolo+'/'+value.indice
+								+' '+value.portata.substring(0,20)+' n. '+value.nr
+								+'</div>');
+				contentId.push(idDiv);		
+			//}			
+		});		
+		
+	});
+	/*var toRemove = _.difference(contentId, gets); 
+	deleteDone(toRemove);*/
+}
+
+function deleteDone(data){
+	$.each(data, function(index, value){
+		$('#'+value).remove();
+	});
+	_.remove(contentId, data);
+}
+
+var color=[
+	'red',
+	'blue',
+	'yellow',
+	'gray',
+	'green',
+	'purple',
+	'lightblue'
+];
+
+function getColorClass(){
+	var _class=color[0];
+	color.push(color.shift());
+	return _class;
+}
