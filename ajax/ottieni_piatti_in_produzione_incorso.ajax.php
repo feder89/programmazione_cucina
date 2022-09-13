43d5dded08991ca41152d00ed1a8405b55e9f6ed
Federico Quaglia <federico.quaglia@thalesgroup.com>
2022-09-14 01:16:18 +0200

    query filtro data

--- ajax/ottieni_piatti_in_produzione_incorso.ajax.php
+++ ajax/ottieni_piatti_in_produzione_incorso.ajax.php
@@ -2,9 +2,14 @@
 	require_once '../include/core.inc.php';
 	$link=connectToDb();
 	$portate=array();
+	$date=ottieni_data_serata_attuale();
+    if($date <=0){
+        $comanda=array('error' => '#error#Errore durante l\'acquisizione della data');
+    }
+    else{
 		$idprg=0;
 		$idprg_next=0;
-		$query_idprog="SELECT min(idprogrammazione) as idprg FROM programmazioneordini WHERE stato = 2 AND categoria IN ('primo','secondo','contorno')";
+		$query_idprog="SELECT min(idprogrammazione) as idprg FROM programmazioneordini WHERE stato = 2 AND categoria IN ('primo','secondo','contorno') AND serata='$date'";
 		$result_idprog = mysqli_query($link, $query_idprog) or die("#error#".mysqli_error($link));
 	    while ($row_idprg = mysqli_fetch_assoc($result_idprog)) {
 	    	$idprg=$row_idprg['idprg'];
@@ -22,6 +27,7 @@
 			WHERE idprogrammazione=$idprg 
 			AND stato=2
 			AND categoria IN ('primo','secondo','contorno')
+			AND serata='$date'
 			GROUP BY portata, tavolo ,indice, idprogrammazione
 			ORDER BY idprogrammazione, tavolo, portata";
 
@@ -36,7 +42,7 @@
 		    }  
 	    }
 		
-
+	}
 	disconnetti_mysql($link, NULL); #visto che non ho un result_set gli passo NULL.. nella funzione in core.in.php ho aggiunto il controllo
 
 	echo json_encode($portate);
